501f6fab11f018c4e7584d86467fdffea30b756a
Marius Jochheim <marius.jochheim@gmail.com>
2025-09-14 15:48:17 +0200

    Implement retry logic for database connection attempts

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"math"
 	"os"
+	"time"
 
 	"github.com/jackc/pgx/v5"
 )
@@ -15,11 +16,22 @@ func main() {
 		log.Fatal("DATABASE_URL environment variable is not set")
 
 	}
-	conn, err := pgx.Connect(context.Background(), connString)
-	if err != nil {
-		log.Fatalf("Unable to connect to database: %v\n", err)
+	var conn *pgx.Conn 
+	var err error
+	maxRetries := 5
+
+	for i:=0; i<maxRetries; i++ {
+		conn, err = pgx.Connect(context.Background(), connString)
+		if err == nil {
+			break
+		}
+		log.Printf("Connection failed:%v. Retrying...", err)
+		time.Sleep(time.Duration(math.Pow(2, float64(i))) * time.Second)
+	}
+	if (err != nil) {
+		log.Fatalf("Unable to connect to database after %d attempts: %v\n", maxRetries, err)
 	}
 	defer conn.Close(context.Background())
 
-	fmt.Println("Successfully connected to the database")
+	log.Println("Successfully connected to the database")
 }
